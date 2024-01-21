f6f0bce7d8f8cc96f9095970ae665ecac4a6b22e
solomon sunday <schinoyerem007@gmail.com>
2024-01-21 23:39:53 +0100

    cleaned up

--- components/HotelChainList.tsx
+++ components/HotelChainList.tsx
@@ -63,89 +63,95 @@ const HotelChainList = ({
         </div>
       </div>
       <ul className="divide-y divide-gray-300">
-        {hotelChains.map((chain) => (
-          <li
-            key={chain.id}
-            className="px-3 py-2 mb-2 bg-white border rounded-lg"
-          >
-            <div className="flex items-center justify-between">
-              <h3 className="text-lg font-semibold">
-                {chain.name} <span className="text-sm">Star</span>
-              </h3>
-              {chain.id !== "0" && (
-                <Menu as="div" className="relative inline-block text-center">
-                  <div>
-                    <Menu.Button className="inline-flex justify-center w-full px-3 py-2 text-sm font-medium text-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                        />
-                      </svg>
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-50 w-40 mt-0 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <div className="px-1 py-1 ">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              onClick={() => {
-                                handleEditHotelChain(chain.id);
-                              }}
-                              className={`${
-                                active
-                                  ? "bg-gray-200 text-black"
-                                  : "text-black-900"
-                              } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                            >
-                              Edit
-                            </button>
-                          )}
-                        </Menu.Item>
-                      </div>
+        {hotelChains.length > 0 ? (
+          hotelChains.map((chain) => (
+            <li
+              key={chain.id}
+              className="px-3 py-2 mb-2 bg-white border rounded-lg"
+            >
+              <div className="flex items-center justify-between">
+                <h3 className="text-lg font-semibold">
+                  {chain.name} <span className="text-sm">Star</span>
+                </h3>
+                {chain.id !== "0" && (
+                  <Menu as="div" className="relative inline-block text-center">
+                    <div>
+                      <Menu.Button className="inline-flex justify-center w-full px-3 py-2 text-sm font-medium text-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                          />
+                        </svg>
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-50 w-40 mt-0 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="px-1 py-1 ">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                onClick={() => {
+                                  handleEditHotelChain(chain.id);
+                                }}
+                                className={`${
+                                  active
+                                    ? "bg-gray-200 text-black"
+                                    : "text-black-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                              >
+                                Edit
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </div>
 
-                      <div className="px-1 py-1">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              onClick={() =>
-                                handleDeleteHotelChainItem(chain.id)
-                              }
-                              className={`${
-                                active
-                                  ? "bg-gray-200 text-white"
-                                  : "text-gray-900"
-                              } group flex w-full items-center rounded-md px-2 py-2 text-sm  text-red-500`}
-                            >
-                              Delete
-                            </button>
-                          )}
-                        </Menu.Item>
-                      </div>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              )}
-            </div>
-          </li>
-        ))}
+                        <div className="px-1 py-1">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <button
+                                onClick={() =>
+                                  handleDeleteHotelChainItem(chain.id)
+                                }
+                                className={`${
+                                  active
+                                    ? "bg-gray-200 text-white"
+                                    : "text-gray-900"
+                                } group flex w-full items-center rounded-md px-2 py-2 text-sm  text-red-500`}
+                              >
+                                Delete
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </div>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                )}
+              </div>
+            </li>
+          ))
+        ) : (
+          <div className="flex items-center justify-center">
+            No hotel chain found!
+          </div>
+        )}
       </ul>
       <CreateModal
         isOpen={openCreate}
